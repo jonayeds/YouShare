c054e6784830df4c8e39461b764bc4d83571d68f
jonayeds <jonayeeds@gmail.com>
2024-08-24 15:43:27 +0600

    added Logout controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -111,9 +111,30 @@ const loginUser = asyncHandler(async (req, res)=>{
         accessToken,
         refreshToken
     }, "User logged in successfully"))
+})
 
+const logOutUser = asyncHandler( async(req, res)=>{
+    await User.findByIdAndDelete(
+        req.user._id,
+        {
+            $set:{
+                refreshToken: undefined
+            }
+        },
+        {
+            new:true
+        }
+    )
+    const options = {
+        httpOnly: true,
+        secure:true
+    }
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User Logged out"))
 
 })
 
-
-export {registerUser, loginUser}
+export {registerUser, loginUser, logOutUser}
