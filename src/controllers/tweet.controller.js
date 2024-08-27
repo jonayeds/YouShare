9ffaab0b36982e918bff8b76cbf43dd74af4ad8c
jonayeds <jonayeeds@gmail.com>
2024-08-27 13:13:30 +0600

    added update tweet controller

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -2,6 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import {Tweet} from "../models/tweet.model.js"
 import {ApiError} from "../utils/apiError.js"
 import {ApiResponse} from "../utils/apiResponse.js"
+import { isValidObjectId } from "mongoose";
 
 const createTweet = asyncHandler(async(req, res)=>{
     const {content} = req.body
@@ -36,6 +37,27 @@ const getUserTweets = asyncHandler(async(req,res)=>{
     )
 })
 
+const updateTweet = asyncHandler(async(req, res)=>{
+    const {tweetId} = req.params
+    const {content} = req.body
+    if(!content){
+        throw new ApiError(400, "Content missing for Update")
+    }
+    if(!isValidObjectId(tweetId)){
+        console.log(tweetId)
+        throw new ApiError(400, "Invalid tweet Id")
+    }
+    const newTweet = await Tweet.findByIdAndUpdate(tweetId, {
+        content
+    })
+    if(!newTweet){
+        throw new ApiError(404, "Tweet not found")
+    }
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, newTweet, "Updated tweet successfully")
+    )
+})
 
-
-export {createTweet, getUserTweets}
+export {createTweet, getUserTweets, updateTweet}
