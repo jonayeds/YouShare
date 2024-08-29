97a6720cc2a055a34d4058f95d6bf0caca29b8f8
jonayeds <jonayeeds@gmail.com>
2024-08-29 12:52:25 +0600

    Added get Video Comments controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -3,9 +3,44 @@ import { Video } from "../models/video.model.js";
 import { ApiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/apiResponse.js";
-import mongoose from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 
-const getVideoComments = asyncHandler(async (req, res) => {});
+const getVideoComments = asyncHandler(async (req, res) => {
+  const {videoId} = req.params
+  const {page=0, limit=10, sortBy="createdAt", sortType="recent"}=  req.query
+  if(!isValidObjectId(videoId)){
+    throw new ApiError(400, "Invalid Video Id")
+  }
+  const comments = await Comment.aggregate([
+    {
+      $match:{
+        video: new mongoose.Types.ObjectId(videoId)
+      }
+    },
+    {
+      $lookup:{
+        from:"users",
+        localField:"owner",
+        foreignField:"_id",
+        as:"s",
+        pipeline:[
+          {
+            $project:{
+              avatar:1,
+              fullName:1
+            }
+          }
+        ]
+      }
+    }
+  ]).sort(`${sortType !== "recent" ? "" : "-"}${sortBy}`).skip(parseInt(page)* parseInt(limit)).limit(parseInt(limit))
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200, comments, "Successfully fetched Video comments")
+  )
+});
 
 const addComment = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
